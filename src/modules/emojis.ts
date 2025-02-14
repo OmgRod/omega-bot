e9e2acff7b50a3cf7dd1bb7fc393a9b706fbc6ae
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-02-14 19:17:37 +0000

    added emojis + port

--- src/modules/emojis.ts
+++ src/modules/emojis.ts
@@ -1,7 +1,11 @@
 const emojiDictionary: Record<string, string> = {
-    checksolid: '1338909544268693544',
-    xmarksolid: '1338911319876833361',
-    hourglass: '1338963250380935309'
+    check: '1338909544268693544',
+    cross: '1338911319876833361',
+    hourglass: '1338963250380935309',
+    ban: '1340036515098660996',
+    hammer: '1340036532458885311',
+    info: '1340038461411037355',
+    warning: '1340039071627477095',
   };
   
   function getEmojiString(name: string): string {
